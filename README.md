d63b262e76f5dc6322bfc261f1816bb7752be4e2
Casey Knolla <cknolla@gmail.com>
2024-06-16 08:26:03 -0700

    add readme, make AddNums generic, and add SubNums

---
+++ README.md
@@ -0,0 +1,1 @@
+Practicing Go workspaces and versioning from [Learning Go](https://github.com/learning-go-book-2e)
--- lib.go
+++ lib.go
@@ -1,5 +1,17 @@
 package workspace_lib
 
-func AddNums(a, b int) int {
+type Number interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr |
+		~float32 | ~float64
+}
+
+// AddNums adds numbers of any numeric type
+func AddNums[T Number](a, b T) T {
 	return a + b
+}
+
+// SubNums can only subtract integers for some reason
+func SubNums(a, b int) int {
+	return a - b
 }
